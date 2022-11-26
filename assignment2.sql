4d1cfed8f72559907bc63ca88d27b967e1ff4fbe
BarborosT <72398699+BarborosT@users.noreply.github.com>
2022-11-27 01:33:54 +0300

    Add files via upload

---
+++ assignment2.sql
@@ -0,0 +1,5 @@
+Select * From film Where replacement_cost Between(12.99 and 16.99)
+
+Select * From actor Where first_name IN ("Penelope","Nick","Ed")
+
+Select * From film where rental_rate In (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99)
